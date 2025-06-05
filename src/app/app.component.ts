7ab9c8a9e4ec57023e1093b8e5a1a8fc4f6c5508
Advaith Chaitanya <advaithchaitanya@gmail.com>
2025-06-05 12:16:08 +0530

    Removed stuff from app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'studentDashboard';
+  students:{name:string; grade : string}[]=[]
+  name:string='';
+  grade:string='';
+  addStudent(){
+    if(this.name && this.grade){
+      const student = {
+        name: this.name,
+        grade: this.grade
+      };
+      console.log('Student added:', student);
+      this.name = '';
+      this.grade = '';
+      this.students.push(student)
+    } else {
+      alert('Please fill in both fields.');
+    }
+  }
 }
